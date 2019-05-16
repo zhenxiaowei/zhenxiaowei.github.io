11699032e60e3551e561ddceca554177542b755e
zhenxiaowei <zhenxiaowei>
2019-05-16 17:01:31 +0800

    update

--- static/js/public.js
+++ static/js/public.js
@@ -5,7 +5,7 @@
     var isIOS = uAgent.match(/iphone/i);
     function resizeRoot() {
         var wWidth = (screen.width > 0) ? (window.innerWidth >= screen.width || window.innerWidth == 0) ? screen.width :
-            window.innerWidth : window.innerWidth,
+                window.innerWidth : window.innerWidth,
             wDpr, wFsize;
         var wHeight = (screen.height > 0) ? (window.innerHeight >= screen.height || window.innerHeight == 0) ?
             screen.height : window.innerHeight : window.innerHeight;
@@ -15,8 +15,8 @@
             wDpr = isIOS ? wWidth > 818 ? 3 : wWidth > 480 ? 2 : 1 : 1;
         }
         // if (isIOS) {
-        wWidth = screen.width;
-        wHeight = screen.height;
+            wWidth = screen.width;
+            wHeight = screen.height;
         // }
         if (wWidth > wHeight) {
             wWidth = wHeight;
